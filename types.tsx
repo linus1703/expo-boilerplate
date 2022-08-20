11d73a5a6f25a9d641f2f7169c6418a8f979257c
Linus Timm <linustimm@live.de>
2022-08-20 11:19:36 +0200

    Setup navigation for auth states

--- types.tsx
+++ types.tsx
@@ -3,9 +3,12 @@
  * https://reactnavigation.org/docs/typescript/
  */
 
-import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
-import { CompositeScreenProps, NavigatorScreenParams } from '@react-navigation/native';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
+import {
+  CompositeScreenProps,
+  NavigatorScreenParams,
+} from "@react-navigation/native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 declare global {
   namespace ReactNavigation {
@@ -14,22 +17,26 @@ declare global {
 }
 
 export type RootStackParamList = {
-  Root: NavigatorScreenParams<RootTabParamList> | undefined;
+  SignedOutState: NavigatorScreenParams<SignedOutParamList> | undefined;
+  SignedInState: NavigatorScreenParams<SignedInParamList> | undefined;
   Modal: undefined;
   NotFound: undefined;
 };
 
-export type RootStackScreenProps<Screen extends keyof RootStackParamList> = NativeStackScreenProps<
-  RootStackParamList,
-  Screen
->;
+export type RootStackScreenProps<Screen extends keyof RootStackParamList> =
+  NativeStackScreenProps<RootStackParamList, Screen>;
 
-export type RootTabParamList = {
+export type SignedInParamList = {
   TabOne: undefined;
   TabTwo: undefined;
 };
 
-export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
-  BottomTabScreenProps<RootTabParamList, Screen>,
-  NativeStackScreenProps<RootStackParamList>
->;
+export type SignedOutParamList = {
+  SignIn: undefined;
+};
+
+export type RootTabScreenProps<Screen extends keyof SignedInParamList> =
+  CompositeScreenProps<
+    BottomTabScreenProps<SignedInParamList, Screen>,
+    NativeStackScreenProps<RootStackParamList>
+  >;
